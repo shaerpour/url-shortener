d2b893b476e11bdc34b1462d55faf5107855953b
shaerpour <amirhosseinshaerpour1@gmail.com>
2024-07-19 12:07:57 +0330

    change - update routes to use db functions

--- internal/routes.go
+++ internal/routes.go
@@ -1,41 +1,43 @@
 package internal
 
 import (
-	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"slices"
 
 	"github.com/gorilla/mux"
 )
 
 func RedirectDomain(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
-
-	for _, val := range DOMAIN_LIST {
-		if val.ID == id {
-			http.Redirect(w, r, val.Domain, http.StatusMovedPermanently)
-			return
-		}
+	url, err := GetDomainByStr(id)
+	if err != nil {
+		log.Fatal(err)
 	}
-	w.WriteHeader(http.StatusBadRequest)
-	json.NewEncoder(w).Encode("Bad request!")
+	http.Redirect(w, r, url, http.StatusMovedPermanently)
 }
 
 func CreateShortener(w http.ResponseWriter, r *http.Request) {
 	var NewURL Domain
 	NewURL.Domain = r.URL.Query().Get("domain")
-	for {
-		UUID := RandomString(10)
-		for _, val := range DOMAIN_LIST {
-			if val.ID == UUID {
+	if id, _ := GetStrByDomain(NewURL.Domain); id != "" {
+		fmt.Fprintf(w, "The domain has been created! Send another domain")
+	} else {
+		for {
+			id = RandomString(10)
+			if slices.Contains(GetAllDomains(), id) {
+				continue
+			} else {
+				NewURL.ID = id
 				break
 			}
 		}
-		NewURL.ID = UUID
-		break
+		err := AddDomain(NewURL.Domain, NewURL.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Shortend URL: http://%s/%s\n", os.Getenv("URL_SHORTENER_URL"), NewURL.ID)
 	}
-
-	DOMAIN_LIST = append(DOMAIN_LIST, NewURL)
-	fmt.Fprintf(w, "Shortend URL: http://%s/%s\n", os.Getenv("URL_SHORTENER_URL"), NewURL.ID)
 }
